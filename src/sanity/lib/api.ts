4e180391ac846690eb50545458cf2830c4d9af71
Gabriel Agbese <gabriel.agbese2@gmail.com>
2025-05-15 11:56:59 +0100

    api.ts explicit set

--- src/sanity/lib/api.ts
+++ src/sanity/lib/api.ts
@@ -1,21 +1,8 @@
 export const useCdn = false;
 export const studioUrl = "/studio";
 
-export const dataset = assertValue(
-    process.env.NEXT_PUBLIC_SANITY_STUDIO_DATASET,
-    "Missing environment variable: NEXT_PUBLIC_SANITY_STUDIO_DATASET"
-);
+export const dataset = process.env.SANITY_STUDIO_DATASET || "production";
 
-export const projectId = assertValue(
-    process.env.NEXT_PUBLIC_SANITY_STUDIO_PROJECT_ID,
-    "Missing environment variable: NEXT_PUBLIC_SANITY_STUDIO_PROJECT_ID"
-);
+export const projectId = process.env.SANITY_STUDIO_PROJECT_ID || "0lkj1dgl";
 
-export const apiVersion = process.env.SANITY_STUDIO_API_VERSION || "2024-04-16";
-
-function assertValue<T>(v: T | undefined, errorMessage: string): T {
-    if (v === undefined) {
-        throw new Error(errorMessage);
-    }
-    return v;
-}
+export const apiVersion = process.env.SANITY_STUDIO_API_VERSION || "2025-04-15";
